d2c9b9a3bdbba3f291bc97605df387d77d74d1d6
a2zincloud <a2zincloud@gmail.com>
2025-02-09 13:51:39 -0500

    Task6-Refactored Code

--- server.py
+++ server.py
@@ -1,13 +1,12 @@
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
-
 app = Flask("Emotion Detector")
-
 @app.route("/emotionDetector")
 def sent_analyzer():
     text_to_analyze = request.args.get('textToAnalyze')
-    response = imotion_detector(text_to_analyze)
+    response = emotion_detector(text_to_analyze)
     result = format_emotion_response(response)
+    return result
 
 @app.route("/")
 def render_index_page():
@@ -20,7 +19,12 @@ def format_emotion_response(emotion_response):
     emotions = [f"'{key}': {value}" for key, value in emotion_response.items() if key != 'dominant_emotion']
     emotions_str = ', '.join(emotions)
     dominant_emotion = emotion_response['dominant_emotion']
-    result_str = f"For the given statement, the system response is {emotions_str}. The dominant emotion is {dominant_emotion}."
+    result_str = (
+    f"For the given statement, the system response is {emotions_str}. "
+    f"The dominant emotion is {dominant_emotion}."
+    )
+
     return result_str
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
